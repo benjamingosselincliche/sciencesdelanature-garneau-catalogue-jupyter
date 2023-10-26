ae05fc36a27d4fd21741249316c49450db9a0e53
Benjamin-GosselinCliche <benjamingcliche@gmail.com>
2023-10-26 14:34:38 -0400

    Create canevas.py

---
+++ catalogue/XXX-ABC-00_Nom_du_cours/scripts/canevas.py
@@ -0,0 +1,40 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,scripts//py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # TITRE
+# ---
+
+# %% [markdown]
+# ## SECTION
+
+# %% [markdown]
+# ### SOUS-SECTION
+
+# %% [markdown]
+# Texte explicatif
+
+# %% [markdown]
+# Texte explicatif image
+# ![title0](img/presentation.png)
+
+# %% [markdown]
+# Le lien est le suivant [example.com](https://example.com/ "https://example.com/").
+
+# %%
+# input 1
+a = 1
+print(bool(1))
+
